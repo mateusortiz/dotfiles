6d14f226c124259e4c50f0ee7e566daf40d1f8e4
mateus ortiz <mateus.prado@ig.com.br>
2015-01-22 21:12:50 -0200

    add Sublime and log functions

---
+++ sublime/install.sh
@@ -0,0 +1,10 @@
+=!/bin/sh
+source $home/.dotfiles/log.sh
+
+# -- Subl ----------------------------------------------------------------------
+if [ -f "/usr/local/bin/subl" ]; then
+	msg_ok "subl"
+else
+	mgs_run "subl" "ln -s '/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl' /usr/local/bin/subl"
+	ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
+fi
