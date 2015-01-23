4d511d57d53cd20e939e9a612bf100c7ef6e9ef2
mateus ortiz <mateus.prado@ig.com.br>
2015-01-22 23:10:48 -0200

    Add Python configurations

---
+++ python/install.sh
@@ -0,0 +1,10 @@
+=!/bin/sh
+source $HOME/.dotfiles/log.sh
+
+# -- Python ----------------------------------------------------------------------
+if which pip &> /dev/null; then
+	msg_ok "pip"
+else
+	msg_run "pip" "sudo easy_install pip"
+	sudo easy_install pip
+fi
