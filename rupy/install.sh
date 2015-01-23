a18903baf323b9b25cb7915794885dc72af750eb
mateus ortiz <mateus.prado@ig.com.br>
2015-01-23 00:00:16 -0200

    Add Ruby configurations and packages

---
+++ rupy/install.sh
@@ -0,0 +1,17 @@
+=!/bin/sh
+source $HOME/.dotfiles/log.sh
+
+# -- Rbenv ----------------------------------------------------------------------
+if which rbenv &> /dev/null; then
+	msg_ok "rbenv"
+else
+	msg_run "rbenv" "git clone https://github.com/sstephenson/rbenv.git $HOME/.rbenv"
+	git clone https://github.com/sstephenson/rbenv.git $HOME/.rbenv
+fi
+# -- Ruby Build ----------------------------------------------------------------------
+if which ruby-build &> /dev/null; then
+	msg_ok "ruby-build"
+else
+	msg_run "ruby-build" "git clone https://github.com/sstephenson/ruby-build.git $HOME/.rbenv/plugins/ruby-build"
+	git clone https://github.com/sstephenson/ruby-build.git $HOME/.rbenv/plugins/ruby-build
+fi
