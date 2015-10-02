db171c91293be7a027baa2337f1cdaeec1e9db38
Mateus Ortiz <mateus.prado@ig.com.br>
2015-10-02 12:42:35 -0300

    Return n for Node Version manager

--- node/install.sh
+++ node/install.sh
@@ -9,10 +9,10 @@ else
 	brew install node
 fi
 
-# -- NVM ----------------------------------------------------------------------
-if which nvm &>/dev/null; then
-	msg_ok "nvm"
+# -- N -------------------------------------------------------------------------
+if which n &> /dev/null; then
+	msg_ok "n"
 else
-	msg_run "nvm" "curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.27.1/install.sh | bash"
-	curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.27.1/install.sh | bash
+	msg_run "n" "sudo npm install -g n"
+	sudo npm install -g n
 fi
