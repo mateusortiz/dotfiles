0c01212ab7cc446780f3ae7fe9e2434cda306959
Mateus Ortiz <mateus.prado@ig.com.br>
2016-06-16 01:47:26 -0300

    Remove packages and fixing install

--- node/install.sh
+++ node/install.sh
@@ -1,18 +1,74 @@
-#!/bin/sh
 source $HOME/.dotfiles/log.sh
 
-# -- Node.JS ----------------------------------------------------------------------
-if which node &>/dev/null; then
+# node.js
+if which node &> /dev/null; then
 	msg_ok "node"
 else
-	msg_run "node" "brew install node"
-	brew install node
+	msg_run "node" "npm install -g n"
+	npm install -g n
+	n latest
+	msg_done "Done"
 fi
 
-# -- N -------------------------------------------------------------------------
-if which n &> /dev/null; then
-	msg_ok "n"
+# bower
+if which bower &> /dev/null; then
+	msg_ok "bower"
 else
-	msg_run "n" "sudo npm install -g n"
-	sudo npm install -g n
+	msg_run "bower" "npm install -g bower"
+	npm install -g bower
+	msg_done "Done"
+fi
+
+# docpad
+if which docpad &> /dev/null; then
+	msg_ok "docpad"
+else
+	msg_run "docpad" " npm install -g docpad"
+	 npm install -g docpad
+	msg_done "Done"
+fi
+
+# grunt-cli
+if which grunt &> /dev/null; then
+	msg_ok "grunt"
+else
+	msg_run "grunt" "npm install -g grunt-cli"
+	npm install -g grunt-cli
+	msg_done "Done"
+fi
+
+# gulp
+if which gulp &> /dev/null; then
+	msg_ok "gulp"
+else
+	msg_run "gulp" "npm install -g gulp"
+	npm install -g gulp
+	msg_done "Done"
+fi
+
+# yeoman
+if which yeoman &> /dev/null; then
+	msg_ok "yeoman"
+else
+	msg_run "jshint" "npm install -g yeoman"
+	npm install -g yeoman
+	msg_done "Done"
+fi
+
+# jshint
+if which jshint &> /dev/null; then
+	msg_ok "jshint"
+else
+	msg_run "jshint" "npm install -g jshint"
+	npm install -g jshint
+	msg_ok "Done"
+fi
+
+# harmonic
+if which harmonic &> /dev/null; then
+	msg_ok "harmonic"
+else
+	msg_run "harmonic" "npm install -g harmonic"
+	npm install -g harmonic
+	msg_ok "Done"
 fi
