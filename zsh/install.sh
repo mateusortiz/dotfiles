b410d5a91f375e0b3ebb9f4d664f6daf50f4c145
Mateus Ortiz <mateus.prado@ig.com.br>
2016-06-17 09:45:28 -0300

    Updates in zsh dotfiles

--- zsh/install.sh
+++ zsh/install.sh
@@ -7,35 +7,29 @@ if which zsh &> /dev/null; then
 else
 	msg_run "zsh" "brew install zsh"
 	brew install zsh
+	msg_done "Done"
 fi
 
 # setting zsh default
 chsh -s /bin/zsh
 
 
-# -- ZSH configs ----------------------------------------------------------------------
+# ZSH configs
 if [[ -d "$HOME/.zshrc" || -L "$HOME/.zshrc" ]]; then
 	msg_ok "zshrc"
 else
 	msg_run "zshrc" "ln -s $HOME/.dotfiles/zsh/.zshrc $HOME/.zshrc"
 	ln -s $HOME/.dotfiles/zsh/.zshrc $HOME/.zshrc
+	msg_done "Done"
 fi
 
-# -- oh my zsh ----------------------------------------------------------------------
+# oh my zsh
 if [[ -d "$HOME/.oh-my-zsh" ]]; then
 	msg_ok "oh-my-zsh"
 else
-	msg_run "oh-my-zsh" "git clone https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh"
-	git clone https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
-fi
-
-# -- plugins ----------------------------------------------------------------------
-if [[ -L $HOME/.oh-my-zsh/custom-dotfiles/plugins ]]; then
-	msg_ok "oh-my-zsh plugins"
-else
-	msg_run "oh-my-zsh plugins" "ln -s $HOME/.dotfiles/zsh/plugins $HOME/.oh-my-zsh/custom-dotfiles/plugins"
-	mkdir -p $HOME/.oh-my-zsh/custom-dotfiles
-	ln -s $HOME/.dotfiles/zsh/plugins $HOME/.oh-my-zsh/custom-dotfiles/plugins
+	msg_run "oh-my-zsh" "curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+	msg_done "Done"
 fi
 
 msg_done "DONE! now reload your Terminal =D"
